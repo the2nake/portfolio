c347fc3bd89e5a4aba895d19cd458280d357d632
the2nake <41810559+the2nake@users.noreply.github.com>
2025-11-07 03:25:44 -0500

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennisklappe.github.io',
+  site: 'https://the2nake.github.io/',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
