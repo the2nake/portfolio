05e2da5e22ff549cff1d113b0888ae38f3c7af90
the2nake <41810559+the2nake@users.noreply.github.com>
2025-11-09 03:34:22 -0500

    changed site root

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://the2nake.github.io/portfolio',
+  site: 'https://the2nake.github.io/portfolio/',
   // The name of your site, used in the title and for SEO.
   title: 'the2nake',
   // The description of your site, used for SEO and RSS feed.
