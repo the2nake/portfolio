c14890cfd86bb6c0dcb5f131733ef88e665da20a
the2nake <41810559+the2nake@users.noreply.github.com>
2025-11-17 10:13:29 -0500

    clean up git cache

--- .astro/content-assets.mjs
+++
@@ -1,7 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Zs4Ge3 from "./PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_1W6MYL from "./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_ZpRc0 from "./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md";
-import __ASTRO_IMAGE_IMPORT_Z1Glz99 from "./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md";
-export default new Map([["./PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_Zs4Ge3], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_1W6MYL], ["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_ZpRc0], ["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z1Glz99]]);
-		
